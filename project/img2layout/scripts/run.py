14d91b090de53c1261bb231ddb216a59e895fb4d
raind <848299610@qq.com>
2025-01-15 21:59:22 +0800

    update img2layout

--- project/img2layout/scripts/run.py
+++ project/img2layout/scripts/run.py
@@ -5,7 +5,6 @@
 import os
 import sys
 import os.path as osp
-from PIL import Image
 
 sys.path.append(osp.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..'))
 from pdf_extract_kit.utils.config_loader import (load_config,
@@ -27,8 +26,18 @@ def main(img, config_path):
     print(extract_results)
 
 
+def demo_img():
+    from PIL import Image
+    import numpy as np
+
+    # 创建一个NumPy数组
+    array = np.array([[255, 255, 255]], dtype=np.uint8)  # 这里是一个1x1的白色像素
+
+    return Image.fromarray(array)
+
+
 if __name__ == '__main__':
     config_path = '../configs/img2layout.yml'
-    img = Image.fromarray([255, 255, 255])
+    img = demo_img()
 
     main(img, config_path=config_path)

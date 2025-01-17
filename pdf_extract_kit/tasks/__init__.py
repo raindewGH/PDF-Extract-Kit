db6b788e18f11ff3907ccdd338fbfbbd43f0d7d2
raind <848299610@qq.com>
2025-01-17 15:19:55 +0800

    update model

--- pdf_extract_kit/tasks/__init__.py
+++ pdf_extract_kit/tasks/__init__.py
@@ -1,19 +1,19 @@
 from pdf_extract_kit.tasks.base_task import BaseTask
-from pdf_extract_kit.tasks.formula_detection.task import FormulaDetectionTask
-from pdf_extract_kit.tasks.formula_recognition.task import FormulaRecognitionTask
+# from pdf_extract_kit.tasks.formula_detection.task import FormulaDetectionTask
+# from pdf_extract_kit.tasks.formula_recognition.task import FormulaRecognitionTask
 from pdf_extract_kit.tasks.layout_detection.task import LayoutDetectionTask
-from pdf_extract_kit.tasks.ocr.task import OCRTask
-from pdf_extract_kit.tasks.table_parsing.task import TableParsingTask
+# from pdf_extract_kit.tasks.ocr.task import OCRTask
+# from pdf_extract_kit.tasks.table_parsing.task import TableParsingTask
 
 from pdf_extract_kit.registry.registry import TASK_REGISTRY
 
 __all__ = [
     "BaseTask",
+    # "FormulaDetectionTask",
+    # "FormulaRecognitionTask",
     "LayoutDetectionTask",
-    "FormulaRecognitionTask",
-    "LayoutDetectionTask",
-    "OCRTask",
-    "TableParsingTask",
+    # "OCRTask",
+    # "TableParsingTask",
 ]
 
 def load_task(name, cfg=None):
